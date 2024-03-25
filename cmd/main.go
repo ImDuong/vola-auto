0a7acc0964900046d5a822940f5b28af8d06ab6f
Duong Tran <shiroyasha699969@gmail.com>
2024-03-25 23:12:14 +0700

    change: use urfave/cli for parsing commands and flags from cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -9,53 +10,96 @@ import (
 	"github.com/ImDuong/vola-auto/config"
 	"github.com/ImDuong/vola-auto/runner"
 	"github.com/ImDuong/vola-auto/utils"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	volatilityPath := flag.String("v", "", "Path to Volatility 3")
-	memDumpPath := flag.String("f", "", "Path to memory dump file")
-	outputFolderPath := flag.String("o", "", "Path to output folder")
-	isForcedRerun := flag.Bool("r", false, "Force to re-run all plugins. Override old results")
-	flag.Parse()
+	cmd := &cli.Command{
+		Name:  "Vola Auto",
+		Usage: "Auto streamline for Volatility 3",
+		Commands: []*cli.Command{
+			{
+				Name:    "dumpfiles",
+				Aliases: []string{"d"},
+				Usage:   "dump files",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Aliases: []string{"r"},
+						Name:    "regex",
+					},
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					// TODO: support regex
+					return nil
+				},
+			},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "vol",
+				Aliases:  []string{"v"},
+				Usage:    "Path to Volatility 3",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Usage:    "Path to memory dump file",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "output",
+				Aliases:  []string{"o"},
+				Usage:    "Path to output folder",
+				Required: true,
+			},
+			&cli.BoolFlag{
+				Name:    "rerun",
+				Aliases: []string{"r"},
+				Usage:   "Force to re-run all plugins. Override old results",
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			pythonRunner, pythonVersion, err := utils.GetPythonRunner()
+			if err != nil {
+				return fmt.Errorf("error when getting python version")
+			}
+			if len(pythonVersion) > 2 && pythonVersion[0] == '2' {
+				return fmt.Errorf("volatility 2 is not supported yet")
+			}
+			config.Default.VolRunConfig.Runner = pythonRunner
 
-	if *volatilityPath == "" || *memDumpPath == "" || *outputFolderPath == "" {
-		flag.Usage()
-		os.Exit(1)
+			config.Default.VolRunConfig.Binary = filepath.Join(cmd.String("v"), "vol.py")
+			config.Default.OutputFolder = cmd.String("o")
+			config.Default.DumpFilesFolder = filepath.Join(config.Default.OutputFolder, "dump_files")
+			config.Default.AnalyticFolder = filepath.Join(config.Default.OutputFolder, "analytics")
+			config.Default.MemoryDumpPath = cmd.String("f")
+			config.Default.IsForcedRerun = cmd.Bool("r")
+
+			err = os.MkdirAll(config.Default.OutputFolder, 0755)
+			if err != nil {
+				return fmt.Errorf("error creating output folder: %w", err)
+			}
+
+			err = os.MkdirAll(config.Default.DumpFilesFolder, 0755)
+			if err != nil {
+				return fmt.Errorf("error creating dump files folder: %w", err)
+			}
+
+			err = os.MkdirAll(config.Default.AnalyticFolder, 0755)
+			if err != nil {
+				return fmt.Errorf("error creating analytic folder: %w", err)
+			}
+
+			err = runner.RunPlugins()
+			if err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
-	pythonRunner, pythonVersion, err := utils.GetPythonRunner()
-	if err != nil {
-		log.Fatalf("Error when getting python version")
-	}
-	if len(pythonVersion) > 2 && pythonVersion[0] == '2' {
-		log.Fatalf("volatility 2 is not supported yet")
-	}
-	config.Default.VolRunConfig.Runner = pythonRunner
-
-	config.Default.VolRunConfig.Binary = filepath.Join(*volatilityPath, "vol.py")
-	config.Default.OutputFolder = *outputFolderPath
-	config.Default.DumpFilesFolder = filepath.Join(config.Default.OutputFolder, "dump_files")
-	config.Default.AnalyticFolder = filepath.Join(config.Default.OutputFolder, "analytics")
-	config.Default.MemoryDumpPath = *memDumpPath
-	config.Default.IsForcedRerun = *isForcedRerun
-
-	err = os.MkdirAll(config.Default.OutputFolder, 0755)
-	if err != nil {
-		log.Fatalf("Error creating output folder: %v\n", err)
-	}
-
-	err = os.MkdirAll(config.Default.DumpFilesFolder, 0755)
-	if err != nil {
-		log.Fatalf("Error creating dump files folder: %v\n", err)
-	}
-
-	err = os.MkdirAll(config.Default.AnalyticFolder, 0755)
-	if err != nil {
-		log.Fatalf("Error creating analytic folder: %v\n", err)
-	}
-
-	err = runner.RunPlugins()
-	if err != nil {
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/ImDuong/vola-auto
 
 go 1.21.5
 
-require github.com/alitto/pond v1.8.3 // indirect
+require (
+	github.com/alitto/pond v1.8.3 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v3 v3.0.0-alpha9 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
